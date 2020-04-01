95923ad7cb1e3250808266988098a2620ac62b47
Thiago Brito <contato@thiagobrito.com.br>
2020-04-01 16:30:17 -0300

    Inserting debug default value.

--- mymoney/settings.py
+++ mymoney/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False)
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
 ALLOWED_HOSTS = ['*']
