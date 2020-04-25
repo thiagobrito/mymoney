b7493430e0221f14d3f5fcf1a3eb30aa28503efc
Thiago Brito <contato@thiagobrito.com.br>
2020-04-25 01:39:46 -0300

    - Inserting a bugged but nice credit card daily estimative of how much I can spend, reducing the amount of money that I've to pay at the beginning with previous bills;

--- mymoney/core/views/summary.py
+++ mymoney/core/views/summary.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from calendar import monthrange
 from django.shortcuts import render
 from django.db.models import Sum
 
@@ -11,19 +12,44 @@ from mymoney.core.models.credit_card import CreditCardBills
 
 
 def view(request, month=None):
-    earnings = Earnings.objects.filter(date__year=datetime.now().year)
-    funds = Funds.objects.filter(date__year=datetime.now().year)
-    expenses = Expenses.objects.filter(date__year=datetime.now().year)
-    unpaid_expenses = Expenses.objects.filter(date__year=datetime.now().year)
-    credit_card = CreditCardBills.objects.filter(payment_date__year=datetime.now().year)
+    def _credit_card_good_daily_estimate(credit_card, month, year, goal=6000):
+        charged_sum = credit_card.filter(charge_count__gt=1).aggregate(Sum('value'))['value__sum']
+
+        number_of_days = monthrange(year, month)[1]
+        return Money((goal - charged_sum) / number_of_days, currency='BRL')
+
+    def _credit_card_month_daily_estimate(credit_card, month, year):
+        number_of_days = monthrange(year, month)[1]
+
+        total_sum = credit_card.aggregate(Sum('value'))['value__sum']
+        charged_sum = credit_card.filter(charge_count__gt=1).aggregate(Sum('value'))['value__sum']
+        return Money((total_sum - charged_sum) / number_of_days, currency='BRL')
+
+
+    year = datetime.now().year
+    earnings = Earnings.objects.filter(date__year=year)
+    funds = Funds.objects.filter(date__year=year)
+    expenses = Expenses.objects.filter(date__year=year)
+    unpaid_expenses = Expenses.objects.filter(date__year=year)
+    credit_card = CreditCardBills.objects.filter(payment_date__year=year)
 
     if month:
         earnings = earnings.filter(date__month=month)
         expenses = expenses.filter(date__month=month)
         unpaid_expenses = unpaid_expenses.filter(date__month=month)
         credit_card = credit_card.filter(payment_date__month=month)
+        credit_card_daily_estimate = _credit_card_good_daily_estimate(credit_card, month, year)
+        credit_card_month_daily_expenses = _credit_card_month_daily_estimate(credit_card, month, year)
+
+        months = ['', 'January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                  'November', 'December']
+        period = months[month] + ' (%s)' % year
+
     else:
         unpaid_expenses = unpaid_expenses.filter(date__month__lte=datetime.now().month).filter(paid=False)
+        credit_card_daily_estimate = None
+        credit_card_month_daily_expenses = None
+        period = year
 
     return render(request, 'index.html',
                   context={
@@ -33,5 +59,9 @@ def view(request, month=None):
                       'expenses': expenses,
                       'expenses_total': Money(expenses.aggregate(total=Sum('value'))['total'] or 0, currency='BRL'),
                       'unpaid_expenses': unpaid_expenses,
-                      'credit_card_total': Money(credit_card.aggregate(total=Sum('value'))['total'] or 0, currency='BRL')
+                      'credit_card_total': Money(credit_card.aggregate(total=Sum('value'))['total'] or 0,
+                                                 currency='BRL'),
+                      'period': period,
+                      'credit_card_daily_estimate': credit_card_daily_estimate,
+                      'credit_card_month_daily_expenses': credit_card_month_daily_expenses
                   })
