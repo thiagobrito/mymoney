0cdc56ea924a7d97e688954e93674cce1b2e39dc
Thiago Brito <contato@thiagobrito.com.br>
2020-10-08 18:36:33 -0300

    melhorias gerais

--- mymoney/core/views/credit_card_estimatives.py
+++ mymoney/core/views/credit_card_estimatives.py
@@ -7,13 +7,14 @@ from mymoney.core import util
 
 
 def good_daily_estimate(credit_card, month_charged_sum, goal=4000):
-    start = util.add_months(credit_card[0].closing_date, -1)
-    end = credit_card[0].closing_date
+    if credit_card:
+        start = util.add_months(credit_card[0].closing_date, -1)
+        end = credit_card[0].closing_date
 
-    number_of_days = (end - start).days
-    if goal < month_charged_sum:
-        return Money(0, currency='BRL')
-    return Money((goal - month_charged_sum) / number_of_days, currency='BRL')
+        number_of_days = (end - start).days
+        if goal >= month_charged_sum:
+            return Money((goal - month_charged_sum) / number_of_days, currency='BRL')
+    return Money(0, currency='BRL')
 
 
 def month_daily_estimate(credit_card, month_charged_sum):
